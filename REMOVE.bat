50db6942168a57f7e3377cfa451b56af4aaff217
Skylar (remote) <alphabet_he@outlook.com>
2024-07-20 12:36:03 -0600

    revive_sigua and rm_bat

--- REMOVE.bat
+++ REMOVE.bat
@@ -1,6 +1,6 @@
-rmdir /s /q C:\\Dev\\SerenityCitadel\\Intermediate
-rmdir /s /q C:\\Dev\\SerenityCitadel\\Binaries
-rmdir /s /q C:\\Dev\\SerenityCitadel\\Saved
-rmdir /s /q C:\\Dev\\SerenityCitadel\\DerivedDataCache
+rmdir /s /q %~dp0\Intermediate
+rmdir /s /q %~dp0\Binaries
+rmdir /s /q %~dp0\Saved
+rmdir /s /q %~dp0\DerivedDataCache
 del SerenityCitadel.sln
 pause
